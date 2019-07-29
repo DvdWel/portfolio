0c5b9fdc7d316f128ddd4e43f258862dace2f27e
Danny Wel <dvdwel@hotmail.nl>
2019-07-29 11:15:19 +0200

    setup react+sass done

---
+++ src/components/app/pages/homepage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./homepage.css"
+
+class Homepage extends React.Component {
+    render() {
+        return <h2>Dit is de Homepage</h2>
+    }
+}
+
+export default Homepage
