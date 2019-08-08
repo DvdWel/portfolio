9a5c599b6980341e8d9552ccd171df7e528dda04
Danny Wel <dvdwel@hotmail.nl>
2019-08-08 11:44:47 +0200

    implementing vanta

---
+++ src/components/app/components/background/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./background.css"
+
+class Background extends React.Component {
+    render() {
+        return (
+            <div id="vantajs" className='wrapper'>
+            </div>
+        );
+    }
+}
+
+export default Background
--- src/components/app/pages/homepage/index.js
+++ src/components/app/pages/homepage/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import "./homepage.css"
 import Title from './title'
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/100/three.min.js'
+import WAVES from 'https://www.vantajs.com/dist/vanta.waves.min.js'
 
 class Homepage extends React.Component {
+    componentDidMount() {
+      this.effect = WAVES({
+        el: "#vantajs",
+        color: 0x1470,
+        shininess: 26.00,
+        waveHeight: 10.00,
+        waveSpeed: 0.50,
+        zoom: 1.44,
+      })
+    }
+
+    componentWillUnmount() {
+      if (this.effect) this.effect.destroy()
+    }
+
     render() {
         return (
             <div id="vantajs" className='wrapper'>
